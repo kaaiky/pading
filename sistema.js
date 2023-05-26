0805ec722805e86e9dff53691c25ee12b059f055
kaaiky <kaiky.s@gsuite.iff.edu.br>
2023-05-26 13:34:10 -0300

    teste

--- sistema.js
+++ sistema.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.listen(3000)
-console.log("servidor rodando...")
+app.listen(3000);
+console.log("servidor rodando...");
